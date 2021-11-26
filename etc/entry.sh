26c8f9c2a077177b46c75a7ee21c828be36ffbb5
Phil Merricks <seffyroff@gmail.com>
2021-11-26 15:58:35 -0800

    adding beta envs

--- etc/entry.sh
+++ etc/entry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
 				+force_install_dir "${STEAMAPPDIR}" \
-				+app_update "${STEAMAPPID}" \
+				+app_update "${STEAMAPPID}" -beta "${STEAMBETANAME}" -betapassword "${STEAMBETAPASSWD}" \
 				+quit
 
 # Change rcon port on first launch, because the default config overwrites the commandline parameter (you can comment this out if it has done it's purpose)
